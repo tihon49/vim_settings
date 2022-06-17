6341508a737c32d50d0245b15e63db7a71bd2b9a
a.tihonov <a.tihonov@aerodisk.ru>
2022-06-17 14:44:32 +0300

    del youcomplete from plugged

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 sudo apt-get install curl
 sudo apt-get install vim
 
+scp -r .vim ~/
+scp .vimrc ~/
 
 # https://github.com/junegunn/vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
